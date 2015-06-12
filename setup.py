4293733ae5a996473019ab9aa4194fb9a7d07a2c
James Pringle <jameskpringle@gmail.com>
2015-06-12 09:43:35 -0400

    Fixed setup.py to be work correctly. Added a doc.

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ setup(
     version='0.1.0',
     author='James K. Pringle',
     author_email='jpringle@jhu.edu',
-    pacakages=['qtools2', 'qtools2.test'],
+    url='http://www.pma2020.org',
+    packages=['qtools2', 'qtools2.test'],
     license='LICENSE.txt',
     description='Useful tools for working with PMA2020 Questionnaires',
-    long_description=open('README.txt').read()
+    long_description=open('README.txt').read(),
     install_requires=[
         'pyxform>=0.9.22'
     ],
