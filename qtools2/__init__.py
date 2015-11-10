e9735a366713d22dc726e161846b682a52d18694
James Pringle <jameskpringle@gmail.com>
2015-11-10 16:36:47 -0500

    Trying GUI file input

--- qtools2/__init__.py
+++ qtools2/__init__.py
@@ -1,5 +1,25 @@
-from qxml import FileChecker
-from qxmledit import Xform 
-from qxmledit import edit_all
+import os
+from Tkinter import Tk
+from tkFileDialog import askopenfilenames
 
 __all__ = ['qxml', 'qxmledit']
+
+import qxml
+
+if __name__ == '__main__':
+    print 'Please select source MS-Excel files for conversion.'
+
+    Tk().withdraw()
+    filenames = askopenfilenames(initialdir=os.getcwd())
+
+    try:
+        if not isinstance(filenames, tuple):
+            m = 'Expected a tuple as returned value from dialog, got %s' % type(filenames)
+            raise TypeError(m)
+        qxml.xlsform_convert(filenames)
+    except TypeError as e:
+        print 'No files picked.'
+    except NameError as e:
+        print 'No files picked.'
+
+    s = input('Press enter to end the program.')
