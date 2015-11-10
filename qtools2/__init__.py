f3e0da09cc3b392f4872cfaaf92c41cdca3f7e87
James Pringle <jameskpringle@gmail.com>
2015-11-10 16:48:22 -0500

    Trying __main__ module

--- qtools2/__init__.py
+++ qtools2/__init__.py
@@ -1,25 +1,5 @@
-import os
-from Tkinter import Tk
-from tkFileDialog import askopenfilenames
+import qxml
+import qxmledit
 
 __all__ = ['qxml', 'qxmledit']
 
-import qxml
-
-if __name__ == '__main__':
-    print 'Please select source MS-Excel files for conversion.'
-
-    Tk().withdraw()
-    filenames = askopenfilenames(initialdir=os.getcwd())
-
-    try:
-        if not isinstance(filenames, tuple):
-            m = 'Expected a tuple as returned value from dialog, got %s' % type(filenames)
-            raise TypeError(m)
-        qxml.xlsform_convert(filenames)
-    except TypeError as e:
-        print 'No files picked.'
-    except NameError as e:
-        print 'No files picked.'
-
-    s = input('Press enter to end the program.')
