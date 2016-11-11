50832e151996e35f0c5d7366b329d28e6d2a52d9
James Pringle <jameskpringle@gmail.com>
2016-11-11 16:44:48 -0500

    Cleaned up scripts directory

--- scripts/xlsform-convert.py
+++ scripts/xlsform-convert.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
+
 from qtools2.qgui import run_conversion
 
-if __name__ == '__main__':
-    run_conversion()
+run_conversion()
+
--- scripts/xlsform-offline.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-from qtools2.qgui import start_gui
-
-# For regular XLSOffline conversion
-start_gui(pma=False)
